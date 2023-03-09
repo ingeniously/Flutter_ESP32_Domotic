1076e23b18730b3fa9c2f00a8de14aac37920843
Cedric-Francois <111455408+ingeniously@users.noreply.github.com>
2023-03-09 12:21:22 +0100

    Add files via upload

---
+++ Lampe.ino
@@ -0,0 +1,86 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>   
+#include <ESP8266HTTPClient.h> 
+#define FIREBASE_HOST "keeper-connect-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "6pTTp2lBqUIYk1XHoxkUaaOhhU71dEbQnmeO0NO6"
+#define WIFI_SSID "Maker Lab"
+#define WIFI_PASSWORD "makerlabtn"                                    
+int val1=0, val2=1000;
+int fireStatus = 0;
+#include <MCP3XXX.h>
+#include<SPI.h>
+MCP3002 adc;
+int x= 0;
+#define acs712 A0
+long lastsample = 0;
+long samplesum = 0;
+int sampleCount = 0;
+float vpc = 4.8828125;
+void setup(){
+   adc.begin(D8, D7, D6, D5);
+Serial.begin(115200);
+pinMode(D4, OUTPUT); 
+        
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);          
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);}
+ Serial.println();
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP Address is : ");
+  Serial.println(WiFi.localIP());                
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);  
+  delay(1000);}
+void loop() {
+if (millis() > lastsample + 1) {
+    int x = adc.analogRead(0)-2;
+   //  Serial.println(x);
+    samplesum += sq(x - 512);
+    sampleCount++;
+    lastsample = millis ();}
+  if (sampleCount == 1000) {
+    float mean = samplesum / sampleCount;
+    float value = sqrt(mean);
+    float mv = value * vpc;
+    float amperage = mv / 66;  //66 for 30A  185 for 20A  185 for 5A
+        samplesum = 0;
+    sampleCount = 0;
+    if (amperage <0.2){
+     amperage =0;
+     Firebase.setString("House/Salon/feedback_Lampe","LOW");
+         Serial.println("The final RMS Amperage is: " + String(amperage) + " Appox WATTAGE is: " + String(amperage * 220) + ".");
+    Firebase.setFloat("House/Salon/Consommation_Lampe",amperage*220);
+      int result = Firebase.getInt("House/Salon/switch");
+  if (result == 1) {
+    Serial.println("Led Turned ON");
+    digitalWrite(D4, LOW);
+}
+  else if (result == 0){
+    Serial.println("Led Turned OFF");
+    digitalWrite(D4, HIGH);
+ }
+    
+    
+    }
+     else{
+       Firebase.setString("House/Salon/feedback_Lampe","HIGH");
+          Serial.println("The final RMS Amperage is: " + String(amperage) + " Appox WATTAGE is: " + String(amperage * 220) + ".");
+    Firebase.setFloat("House/Salon/Consommation_Lampe",amperage*220);
+
+  int result = Firebase.getInt("House/Salon/switch");
+  if (result == 1) {
+    Serial.println("Led Turned ON");
+    digitalWrite(D4, LOW);
+}
+  else if (result == 0){
+    Serial.println("Led Turned OFF");
+    digitalWrite(D4, HIGH);
+ }
+    
+      } 
+  }
+    
+  }
